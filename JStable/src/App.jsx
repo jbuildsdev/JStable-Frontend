3bbf153763880250af702406c5e288fa5f33d90e
J Builds <jbuildsdev@outlook.com>
2023-12-12 22:01:38 -0600

    Connected to JSTB contract with web3

--- JStable/src/App.jsx
+++ JStable/src/App.jsx
@@ -1,10 +1,14 @@
 import Web3 from "web3";
 import { useState } from "react";
+import { JSTB_ADDRESS, JSTB_ABI } from "./contracts/JStable";
 
 function App() {
   const [account, setAccount] = useState(null);
   const [web3, setWeb3] = useState(null);
   const [balance, setBalance] = useState(null);
+  const [contract, setContract] = useState(null);
+  const [mintAddress, setMintAddress] = useState("");
+  const [mintAmount, setMintAmount] = useState("");
 
   const connectToMetamask = async () => {
     console.log("Connecting to MetaMask...");
@@ -50,12 +54,55 @@ function App() {
     }
   };
 
+  const connectToContract = async () => {
+    if (web3) {
+      const contract = new web3.eth.Contract(JSTB_ABI, JSTB_ADDRESS);
+      setContract(contract);
+      console.log("Contract set:", contract);
+    } else {
+      console.log(
+        "Web3 is not initialized. Please connect to MetaMask and initialize Web3 first."
+      );
+    }
+  };
+
+  const mintTokens = async () => {
+    if (contract && mintAddress && mintAmount) {
+      try {
+        await contract.methods
+          .mint(mintAddress, mintAmount)
+          .send({ from: account });
+        console.log("Minting successful");
+      } catch (error) {
+        console.error("Error occurred:", error);
+      }
+    } else {
+      console.log(
+        "Contract, mint address or mint amount is not set. Please connect to the contract and enter the mint address and amount first."
+      );
+    }
+  };
+
   return (
     <div>
       <button onClick={connectToMetamask}>Connect to MetaMask</button>
       <button onClick={getWeb3}>Get Web3</button>
       <button onClick={getBalance}>Get Balance</button>
+      <button onClick={connectToContract}>Connect to Contract</button>
       {balance && <p>Your balance is {balance} ETH</p>}
+      <input
+        type="text"
+        placeholder="Mint Address"
+        value={mintAddress}
+        onChange={(e) => setMintAddress(e.target.value)}
+      />
+      <input
+        type="number"
+        placeholder="Mint Amount"
+        value={mintAmount}
+        onChange={(e) => setMintAmount(e.target.value)}
+      />
+      <button onClick={mintTokens}>Mint Tokens</button>
     </div>
   );
 }
